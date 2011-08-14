519d2ddc5eebbc00271bd760826d80cf9c9c7a23
Laurent Clouet <laurent35240@gmail.com>
2011-08-15 01:00:48 +0200

    Bootstrap for unit tests

---
+++ test/unit/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * @category   Laurent
+ * @package    Laurent_Prerender
+ * @copyright  Copyright (c) 2011 Laurent Clouet
+ * @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ * @author     Laurent Clouet <laurent35240@gmail.com>
+ */
+
+ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . dirname(__FILE__) . '/../../' . PATH_SEPARATOR . dirname(__FILE__));
+ini_set('memory_limit', '512M');
+require_once 'app/Mage.php';
+
+Mage::app('default');
+
+session_start();
+?>
